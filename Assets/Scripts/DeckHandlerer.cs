83e21001bc20b617941506d38b34a90fbe3a26ab
Oliver Tomáš Cenker <olivercenker4@gmail.com>
2025-03-30 03:09:30 +0200

    Add milions of cards and go to sleep

--- Assets/Scripts/DeckHandlerer.cs
+++ Assets/Scripts/DeckHandlerer.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class DeckHandlerer : MonoBehaviour
 {
+    [SerializeField] List<GameObject> smallPlusCards;
+    [SerializeField] List<GameObject> bigPlusCards;
+    [SerializeField] List<GameObject> smallMinusCards;
+    [SerializeField] List<GameObject> bigMinusCards;
+    [SerializeField] List<GameObject> multiplicationCards;
+    [SerializeField] List<GameObject> divisionCards;
+    [SerializeField] List<GameObject> ceasarCards;
+
     private GameSession gameSession;
     private List<GameSession.CardType> deck;
 
@@ -26,5 +34,24 @@ public class DeckHandlerer : MonoBehaviour
     {
         // Make it not visible
         // Display overlay with all possible cards in the middle of the Screen
+    }
+
+    public void OnValueCardClicked()
+    {
+        OnValueCardClickedHelper(smallPlusCards);
+        OnValueCardClickedHelper(bigPlusCards);
+        OnValueCardClickedHelper(smallMinusCards);
+        OnValueCardClickedHelper(bigMinusCards);
+        OnValueCardClickedHelper(multiplicationCards);
+        OnValueCardClickedHelper(divisionCards);
+        OnValueCardClickedHelper(ceasarCards);
+    }
+
+    private void OnValueCardClickedHelper(List<GameObject> cards)
+    {
+        foreach (var card in cards)
+        {
+            card.SetActive(false);
+        }
     }
 }
